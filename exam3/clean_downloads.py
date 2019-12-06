e0a3a4a678a9e5690352d131f700282783cb3c59
guillaume andronic <guillaumeandronic@gmail.com>
2019-12-06 15:43:06 +0100

    exam3

--- exam3/clean_downloads.py
+++ exam3/clean_downloads.py
@@ -1,4 +1,30 @@
+"""
 import os, time, sys
 winuser = os.popen('/mnt/c/Windows/System32/cmd.exe /c "echo %USERNAME%"').read()
 #print(winuser)
 path_to_watch = "/mnt/c/Users/" + winuser[:-1] + "/Downloads/"
+"""
+#!/usr/bin/python3
+import os
+
+winuser = os.popen('/mnt/c/Windows/System32/cmd.exe /c "echo %USERNAME%"').read()
+path_to_watch = "/mnt/c/Users/" + winuser[:-1] + "/Downloads/"
+list_files = os.listdir("/mnt/c/Users/" + winuser[:-1] + "/Downloads/")
+# list_files = os.listdir(".") # repertoire courant
+# print(list_files)
+print(type(list_files))
+"""
+print(list_files[0])
+print(list_files[1])
+"""
+print(list_files[0:99])
+# trouver la taille des fichiers
+list_files[0:99]
+# avec os.stat trouver la taille
+# os.stat(list_files[2]) #ne fonctionne pas car il manque le chemin
+os.stat("/mnt/c/Users/" + winuser[:-1] + "/Downloads/" + list_files[0:99]) # fonctionne ok
+print("toutes les informations du fichier: ", os.stat("/mnt/c/Users/" + winuser[:-1] + "/Downloads/" + list_files[0:99]))
+print("id du owner du fichier: ", os.stat("/mnt/c/Users/" + winuser[:-1] + "/Downloads/" + list_files[0:99]).st_uid)
+print("taille      du fichier: ", os.stat("/mnt/c/Users/" + winuser[:-1] + "/Downloads/" + list_files[0:99]).st_size)
+from pwd import getpwuid
+print("txt  owner  du fichier:", getpwuid(os.stat("/mnt/c/Users/" + winuser[:-1] + "/Downloads/" + list_files[0:99]).st_uid).pw_name)
